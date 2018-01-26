e96e7f9bf9b60df76a480d7b604989624d01c30a
Plurdis <uutak2000@naver.com>
2018-01-26 18:35:44 +0900

    서비스 'Google Custom Search' 추가

---
+++ PlurCrawler/Search/Services/GoogleCSE/GoogleCSESearchOption.cs
@@ -0,0 +1,18 @@
+﻿using PlurCrawler.Search.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlurCrawler.Search.Services.GoogleCSE
+{
+    public class GoogleCSESearchOption : ISearchOption
+    {
+        public string Query { get; set; }
+
+        public ulong SearchCount { get; set; }
+
+        public ulong Offset { get; set; }
+    }
+}
