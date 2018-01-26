f73886dbef2a03200bba045d704498966d06e473
Plurdis <uutak2000@naver.com>
2018-01-26 15:14:10 +0900

    UI 추가 구현 (인증 관련 부분)

---
+++ PlurCrawler_Sample/Controls/AuthStateToolTip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PlurCrawler_Sample.Controls
+{
+    public class AuthStateToolTip : ToolTip
+    {
+        public static DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(AuthStateToolTip));
+
+        public static DependencyProperty DescriptionProperty = DependencyProperty.Register(nameof(Description), typeof(string), typeof(AuthStateToolTip));
+        
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+    }
+}
