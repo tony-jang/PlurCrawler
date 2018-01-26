b8d705e9a6b6f4a280a9239efbe9d3e0abbe14af
Plurdis <uutak2000@naver.com>
2018-01-26 18:56:45 +0900

    서비스 'Google Custom Search' 추가

---
+++ PlurCrawler/Search/Services/GoogleCSESearchResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PlurCrawler.Search.Base;
+
+namespace PlurCrawler.Search.Services.GoogleCSE
+{
+    /// <summary>
+    /// 구글의 Custom Search 검색 결과입니다.
+    /// </summary>
+    public class GoogleCSESearchResult : ISearchResult
+    {
+        /// <summary>
+        /// 해당 검색 결과가 올라온 날짜를 나타냅니다. null일 수 있습니다.
+        /// </summary>
+        public DateTime? PublishedDate { get; set; }
+
+        /// <summary>
+        /// 해당 검색 결과의 원본 URL을 나타냅니다.
+        /// </summary>
+        public string OriginalURL { get; set; }
+
+        /// <summary>
+        /// 해당 검색 결과의 제목을 나타냅니다.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 해당 검색 결과를 미리보기 합니다.
+        /// </summary>
+        public string Snippet { get; set; }
+
+    }
+}
---
+++ PlurCrawler/Search/Services/GoogleCSESearcher.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+using PlurCrawler.Search.Base;
+using PlurCrawler.Search.Utility;
+using PlurCrawler.Tokens.Tokenizer;
+
+namespace PlurCrawler.Search.Services.GoogleCSE
+{
+    public class GoogleCSESearcher : ISearcher
+    {
+        /// <summary>
+        /// <see cref="GoogleCSESearcher"/> 클래스를 초기화합니다.
+        /// </summary>
+        public GoogleCSESearcher()
+        {
+        }
+
+        /// <summary>
+        /// <see cref="GoogleCSESearcher"/> 클래스를 API Key와 검색 엔진 ID로 초기화합니다.
+        /// </summary>
+        /// <param name="apiKey">API Key입니다.</param>
+        /// <param name="searchEngineId">검색 엔진 ID입니다.</param>
+        public GoogleCSESearcher(string apiKey, string searchEngineId)
+        {
+            Vertification(apiKey, searchEngineId);
+        }
+
+        public bool IsVerification { get; private set; }
+
+        public string ApiKey { get; private set; }
+
+        public string SearchEngineId { get; private set; }
+
+        /// <summary>
+        /// ApiKey와 SearchEngineId를 새로 고칩니다. 유효성 검사는 하지 않습니다.
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="searchEngineId"></param>
+        public void Vertification(string apiKey, string searchEngineId)
+        {
+            this.ApiKey = apiKey;
+            this.SearchEngineId = searchEngineId;
+
+            IsVerification = true;
+        }
+
+        /// <summary>
+        /// Google Custom Search를 이용해 검색을 실시합니다. <see cref="ISearcher"/>에서 상속 받은 함수 입니다.
+        /// </summary>
+        /// <param name="searchOption">구글 검색의 검색 옵션입니다. <see cref="GoogleCSESearchOption"/>이 필요합니다.</param>
+        /// <returns></returns>
+        public List<ISearchResult> Search(ISearchOption searchOption)
+        {
+            if (searchOption is GoogleCSESearchOption googleSearchOption)
+            {
+                return Search(googleSearchOption).Select(i => (ISearchResult)i).ToList();
+            }
+            else
+            {
+                throw new SearchOptionTypeException("GoogleCSESearchOption만 넣을 수 있습니다.");
+            }
+        }
+
+        /// <summary>
+        /// Google Custom Search를 이용해 검색을 실시합니다.
+        /// </summary>
+        /// <param name="searchOption">Google Custom Search의 검색 옵션입니다.</param>
+        /// <returns></returns>
+        public List<GoogleCSESearchResult> Search(GoogleCSESearchOption searchOption)
+        {
+            var list = new List<GoogleCSESearchResult>();
+            var customSearchService = new CustomsearchService(new BaseClientService.Initializer
+            {
+                ApiKey = this.ApiKey
+            });
+
+            long targetCount = (long)searchOption.SearchCount;
+
+            CseResource.ListRequest request = customSearchService.Cse.List(searchOption.Query);
+
+            request.Cx = SearchEngineId;
+            var results = new List<Result>();
+            IList<Result> paging = new List<Result>();
+            int count = 0;
+
+            while ((paging != null) && targetCount > 0)
+            {
+                request.Start = count * 10 + 1;
+                if ((targetCount % 10) == 0)
+                    request.Num = 10;
+                else
+                    request.Num = (targetCount % 10);
+
+                paging = request.Execute().Items;
+
+                if (paging != null)
+                    results.AddRange(paging);
+
+                count++;
+                targetCount -= request.Num.Value;
+            }
+
+            if (results.Count == 0)
+                return null;
+
+            return results.Select(i => new GoogleCSESearchResult()
+            {
+                OriginalURL = i.Link,
+                PublishedDate = null,
+                Title = i.Title,
+                Snippet = i.Snippet.Replace("\\n", Environment.NewLine)
+            }).ToList();
+        }
+    }
+}
