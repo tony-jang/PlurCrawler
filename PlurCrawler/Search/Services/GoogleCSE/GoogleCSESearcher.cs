afbf36bfadc9ba6daba954bfbaecf0e0483cc9bb
Plurdis <uutak2000@naver.com>
2018-01-26 19:09:58 +0900

    GoogleCSESearcher XML 문서 주석 수정

--- PlurCrawler/Search/Services/GoogleCSE/GoogleCSESearcher.cs
+++ PlurCrawler/Search/Services/GoogleCSE/GoogleCSESearcher.cs
@@ -23,24 +23,33 @@ namespace PlurCrawler.Search.Services.GoogleCSE
         /// <summary>
         /// <see cref="GoogleCSESearcher"/> 클래스를 API Key와 검색 엔진 ID로 초기화합니다.
         /// </summary>
-        /// <param name="apiKey">API Key입니다.</param>
+        /// <param name="apiKey">API Key 입니다.</param>
         /// <param name="searchEngineId">검색 엔진 ID입니다.</param>
         public GoogleCSESearcher(string apiKey, string searchEngineId)
         {
             Vertification(apiKey, searchEngineId);
         }
 
+        /// <summary>
+        /// Google CSE가 ApiKey와 SearchEngineId가 할당되었는지에 대한 여부를 가져옵니다.
+        /// </summary>
         public bool IsVerification { get; private set; }
 
+        /// <summary>
+        /// Api Key 입니다.
+        /// </summary>
         public string ApiKey { get; private set; }
 
+        /// <summary>
+        /// 검색 엔진 ID 입니다.
+        /// </summary>
         public string SearchEngineId { get; private set; }
 
         /// <summary>
         /// ApiKey와 SearchEngineId를 새로 고칩니다. 유효성 검사는 하지 않습니다.
         /// </summary>
-        /// <param name="apiKey"></param>
-        /// <param name="searchEngineId"></param>
+        /// <param name="apiKey">Api Key 입니다.</param>
+        /// <param name="searchEngineId">검색 엔진 ID 입니다.</param>
         public void Vertification(string apiKey, string searchEngineId)
         {
             this.ApiKey = apiKey;
