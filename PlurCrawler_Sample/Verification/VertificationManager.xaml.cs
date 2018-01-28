1ec617dd248654c58a337e70ee11fd3e55a4c264
Plurdis <uutak2000@naver.com>
2018-01-28 18:45:56 +0900

    인증 매니저 형식 변경

--- PlurCrawler_Sample/Verification/VertificationManager.xaml.cs
+++ PlurCrawler_Sample/Verification/VertificationManager.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PlurCrawler_Sample.Verification
 {
     /// <summary>
-    /// VerificationManager.xaml에 대한 상호 작용 논리
+    /// VertificationManager.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class VerificationManager : Window
+    public partial class VertificationManager : Page
     {
-        public VerificationManager()
+        public VertificationManager()
         {
             InitializeComponent();
         }
