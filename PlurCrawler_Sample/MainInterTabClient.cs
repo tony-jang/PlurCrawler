9d7bd9e33046094c1d7979bbc1b53ce6d6c9d9f2
Plurdis <uutak2000@naver.com>
2018-01-28 16:47:36 +0900

    분리 가능한 TabItem 추가

---
+++ PlurCrawler_Sample/MainInterTabClient.cs
@@ -0,0 +1,24 @@
+﻿using Dragablz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PlurCrawler_Sample
+{
+    class MainInterTabClient : IInterTabClient
+    {
+        public INewTabHost<Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
+        {
+            var view = new TabHostWindow();
+            return new NewTabHost<TabHostWindow>(view, view.TabsContainer);
+        }
+
+        public TabEmptiedResponse TabEmptiedHandler(TabablzControl tabControl, Window window)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PlurCrawler_Sample/MainWindow.xaml.cs
+++ PlurCrawler_Sample/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Dragablz;
 using Newtonsoft.Json;
 using PlurCrawler;
 using PlurCrawler.Search;
@@ -32,8 +32,11 @@ namespace PlurCrawler_Sample
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
             this.Loaded += MainWindow_Loaded;
         }
+
+
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
