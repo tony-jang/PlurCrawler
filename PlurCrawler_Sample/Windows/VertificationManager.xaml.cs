24df66e129a17630ed426043b1ca3e49aa14d5aa
Plurdis <uutak2000@naver.com>
2018-02-03 01:02:34 +0900

    트위터 인증 구현 및 유튜브쪽 복사

--- PlurCrawler_Sample/Windows/VertificationManager.xaml.cs
+++ PlurCrawler_Sample/Windows/VertificationManager.xaml.cs
@@ -1,5 +1,4 @@
-﻿using PlurCrawler_Sample.Enums;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using PlurCrawler.Tokens.Credentials;
+using PlurCrawler.Tokens.Tokenizer;
+using PlurCrawler_Sample.Common;
 
 namespace PlurCrawler_Sample.Windows
 {
@@ -29,35 +32,85 @@ namespace PlurCrawler_Sample.Windows
 
             btnGoogleOK.Click += BtnGoogleOK_Click;
             btnGoogleViewHidden.Click += BtnGoogleViewHidden_Click;
-        }
-        
-        private void BtnGoogleOK_Click(object sender, RoutedEventArgs e)
-        {
-            SetGoogleKey(tbGoogleKey.Text);
+            btnTwitterPINAuth.Click += BtnTwitterPINAuth_Click;
+
+            btnTwitterReqURL.Click += BtnTwitterReqURL_Click;
         }
 
-        private void BtnGoogleViewHidden_Click(object sender, RoutedEventArgs e)
+        #region [  Twitter  ]
+
+        TwitterTokenizer tokenizer;
+        TwitterCredentials credentials;
+
+        private void BtnTwitterPINAuth_Click(object sender, RoutedEventArgs e)
         {
-            if (IsGoogleHidden)
-                runGoogleAPIKey.Text = GoogleAPIKey;
+            tcTwitterAuth.SelectedIndex = 2;
+            string pinNumber = tbTwitterPIN.Text;
+
+            credentials.InputPIN(pinNumber);
+            tokenizer.CredentialsCertification(credentials);
+
+            wbTwitter.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnTwitterReqURL_Click(object sender, RoutedEventArgs e)
+        {
+            string errMsg1 = "잘못된 Consumer Key 또는 Consumer Secret이 입력되었습니다.";
+            string errMsg2 = "Consumer Key또는 Consumer Secret은 빈칸일 수 없습니다.";
+
+            if (string.IsNullOrEmpty(tbTwitterKey.Text) || string.IsNullOrEmpty(tbTwitterSecret.Password))
+            {
+                tbTwitterMsg.Visibility = Visibility.Visible;
+                tbTwitterMsg.Text = errMsg2;
+
+                return;
+            }
+
+            credentials = new TwitterCredentials(tbTwitterKey.Text, tbTwitterSecret.Password);
+            tokenizer = new TwitterTokenizer();
+
+            string url = tokenizer.GetURL(credentials);
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                tbTwitterMsg.Visibility = Visibility.Hidden;
+
+                runTwitterID.Text = tbTwitterKey.Text;
+                runTwitterSecret.Text = runTwitterSecret.Text; 
+
+                tcTwitterAuth.SelectedIndex = 1;
+                wbTwitter.Navigate(url);
+            }
             else
-                runGoogleAPIKey.Text = HiddenText;
-
-            IsGoogleHidden = !IsGoogleHidden;
+            {
+                tbTwitterMsg.Text = errMsg1;
+                tbTwitterMsg.Visibility = Visibility.Visible;
+            }
         }
 
+        #endregion
 
         #region [  Google CSE  ]
 
         private bool IsGoogleHidden { get; set; }
 
+        /// <summary>
+        /// 구글의 API 키를 나타냅니다.
+        /// </summary>
         public string GoogleAPIKey { get; private set; }
 
-        public void SetGoogleKey(string key)
+        /// <summary>
+        /// 구글의 엔진 ID를 나타냅니다.
+        /// </summary>
+        public string GoogleEngineID { get; private set; }
+
+        public void SetGoogleKey(string key, string googleEngineId)
         {
             tbGoogleKey.Clear();
 
             GoogleAPIKey = key;
+            GoogleEngineID = googleEngineId;
+
             IsGoogleHidden = true;
 
             runGoogleAPIKey.Text = HiddenText;
@@ -86,10 +139,32 @@ namespace PlurCrawler_Sample.Windows
             }
             runGoogleAPIKey.Foreground = brush;
             runGoogleAvailable.Foreground = brush;
+            runEngineID.Foreground = brush;
 
             runGoogleAvailable.Text = text;
         }
 
+        private void BtnGoogleOK_Click(object sender, RoutedEventArgs e)
+        {
+            SetGoogleKey(tbGoogleKey.Text, tbEngineID.Text);
+        }
+
+        private void BtnGoogleViewHidden_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsGoogleHidden)
+                runGoogleAPIKey.Text = GoogleAPIKey;
+            else
+                runGoogleAPIKey.Text = HiddenText;
+
+            IsGoogleHidden = !IsGoogleHidden;
+        }
+
+
         #endregion
+
+        private void Button_Click()
+        {
+
+        }
     }
 }
