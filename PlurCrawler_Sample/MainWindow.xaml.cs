296bd9b7cdd8c56e79430069baf93cfe4b452694
Plurdis <uutak2000@naver.com>
2018-02-03 01:03:32 +0900

    샘플 코드 추가 및 UI 배치 변경

--- PlurCrawler_Sample/MainWindow.xaml.cs
+++ PlurCrawler_Sample/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ using PlurCrawler.Search.Services.Twitter;
 using PlurCrawler.Search.Services.Youtube;
 using PlurCrawler.Tokens.Credentials;
 using PlurCrawler.Tokens.Tokenizer;
+using PlurCrawler_Sample.Windows;
 
 namespace PlurCrawler_Sample
 {
@@ -29,16 +30,53 @@ namespace PlurCrawler_Sample
     /// </summary>
     public partial class MainWindow : Window
     {
+        DetailsOption _detailsOption;
+        VertificationManager _vertificationManager;
+
         public MainWindow()
         {
             InitializeComponent();
             this.Loaded += MainWindow_Loaded;
+
+            btnSearch.Click += BtnSearch_Click;
         }
+        
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var googleCSESearcher = new GoogleCSESearcher();
 
+            googleCSESearcher.Vertification(_vertificationManager.GoogleAPIKey, _vertificationManager.GoogleEngineID);
 
+            mainTabControl.SelectedIndex = 0;
+
+            lvLog.Items.Add("[System] 검색을 시작합니다.");
+
+            var list = googleCSESearcher.Search(new GoogleCSESearchOption()
+            {
+                SearchCount = 10,
+                Query = "네이버"
+            });
+
+            lvLog.Items.Add("[System] 검색이 완료되었습니다.");
+
+            foreach (var itm in list)
+            {
+                lvLog.Items.Add(new ListViewItem()
+                {
+                    Content = itm.Title
+                });
+            }
+        }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            #region [  Initalization  ]
+
+            _detailsOption = (DetailsOption)frOption.Content;
+            _vertificationManager = (VertificationManager)frVertManager.Content;
+
+            #endregion
+
             //var cred = new TwitterCredentials("hKomvFO7HT0ZNZM9Kc2lnKhsY", "mk2AoM6iHKuSPmnvsqyHJuKXqIsDVHD37hoB3KA6Y6oksNDhyD");
 
             //TwitterTokenizer tokenizer = new TwitterTokenizer();
