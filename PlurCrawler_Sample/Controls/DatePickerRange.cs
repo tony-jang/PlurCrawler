3f598a92822107580b2dcb139053da8a9fed2382
Plurdis <uutak2000@naver.com>
2018-02-03 01:03:06 +0900

    DatePickerRange 컨트롤 디자인 및 코드 변경

--- PlurCrawler_Sample/Controls/DatePickerRange.cs
+++ PlurCrawler_Sample/Controls/DatePickerRange.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,32 @@ using WPFExtension;
 
 namespace PlurCrawler_Sample.Controls
 {
-    class DatePickerRange : Control
+    [TemplatePart(Name = "dpSince", Type = typeof(DatePicker))]
+    [TemplatePart(Name = "dpUntil", Type = typeof(DatePicker))]
+    class DateRangePicker : Control
     {
+        DatePicker dpSince, dpUntil;
+
+        public DateRangePicker()
+        {
+            this.Style = (Style)FindResource("DateRangePickerStyle");
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            dpSince = base.GetTemplateChild("dpSince") as DatePicker;
+            dpUntil = base.GetTemplateChild("dpUntil") as DatePicker;
+
+
+            dpSince.DisplayDateStart = new DateTime(2005, 1, 1);
+            dpUntil.DisplayDateStart = new DateTime(2005, 1, 1);
+
+            dpSince.DisplayDateEnd = DateTime.Today;
+            dpUntil.DisplayDateEnd = DateTime.Today;
+        }
+
         public static DependencyProperty PropertyNameProperty = DependencyHelper.Register();
         public static DependencyProperty StartDateTimeProperty = DependencyHelper.Register();
         public static DependencyProperty EndDateTimeProperty = DependencyHelper.Register();
