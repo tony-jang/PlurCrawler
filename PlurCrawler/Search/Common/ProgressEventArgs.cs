944f63a82f3fa011dda986f9b5577da26f91f6ab
Plurdis <uutak2000@naver.com>
2018-02-04 20:09:36 +0900

    'SearchProgressChanged' 이벤트 시범 도입

---
+++ PlurCrawler/Search/Common/ProgressEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlurCrawler.Search.Common
+{
+    public class ProgressEventArgs : EventArgs
+    {
+        public ProgressEventArgs(int maximum, int value)
+        {
+            this.Maximum = maximum;
+            this.Value = value;
+        }
+
+        public int Maximum { get; }
+
+        public int Value { get; }
+    }
+}
