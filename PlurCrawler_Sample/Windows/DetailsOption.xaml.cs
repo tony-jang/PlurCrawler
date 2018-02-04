857e9b51a4ef4758c485f606b74eed7bb0572020
Plurdis <uutak2000@naver.com>
2018-02-04 19:56:41 +0900

    DetailsOption 구글 CSE 불러오기/내보내기 추가

--- PlurCrawler_Sample/Windows/DetailsOption.xaml.cs
+++ PlurCrawler_Sample/Windows/DetailsOption.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PlurCrawler.Search.Services.GoogleCSE;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using PlurCrawler.Common;
+
 namespace PlurCrawler_Sample.Windows
 {
     /// <summary>
@@ -23,6 +26,47 @@ namespace PlurCrawler_Sample.Windows
         public DetailsOption()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 구글 파트에서 설정을 불러옵니다.
+        /// </summary>
+        /// <param name="option"></param>
+        public void LoadGoogle(GoogleCSESearchOption option)
+        {
+            drpGoogle.Since = option.DateRange.Since.GetValueOrDefault();
+            drpGoogle.Until = option.DateRange.Until.GetValueOrDefault();
+            tbGooglePageOffset.Text = option.Offset.ToString();
+            tbGoogleSearchCount.Text = option.SearchCount.ToString();
+            if (option.SplitWithDate)
+                rbGoogleSplitWithDate.IsChecked = true;
+            else
+                rbGoogleNoSplit.IsChecked = true;
+        }
+
+        public GoogleCSESearchOption GetGoogleCSESearchOption()
+        {
+            return new GoogleCSESearchOption()
+            {
+                DateRange = new DateRange(drpGoogle.Since, drpGoogle.Until),
+                Offset = ulong.Parse(tbGooglePageOffset.Text),
+                SplitWithDate = rbGoogleSplitWithDate.IsChecked.GetValueOrDefault(),
+                SearchCount = ulong.Parse(tbGoogleSearchCount.Text),
+            };
+        }
+
+        public void GoogleEnableChange(bool enable)
+        {
+            if (enable)
+            {
+                svGoogle.IsEnabled = true;
+                signGoogle.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                svGoogle.IsEnabled = false;
+                signGoogle.Visibility = Visibility.Visible;
+            }
         }
     }
 }
