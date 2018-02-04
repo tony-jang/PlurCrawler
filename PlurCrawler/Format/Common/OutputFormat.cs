09d56927e81d6f8aa683e5706b4d38e773c843d6
Plurdis <uutak2000@naver.com>
2018-02-04 19:58:16 +0900

    Enum 'OutputFormat' 추가

---
+++ PlurCrawler/Format/Common/OutputFormat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlurCrawler.Format.Common
+{
+    public enum OutputFormat
+    {
+        CSV = 1,
+        Json = 2,
+        MySQL = 4,
+        MSSQL = 8,
+    }
+}
