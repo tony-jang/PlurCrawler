9fb681dba39ec49470ce6a716c41089134104a80
Plurdis <uutak2000@naver.com>
2018-02-04 20:10:52 +0900

    출력 방법 'JsonFormat' 추가

---
+++ PlurCrawler/Format/JsonFormat.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using PlurCrawler.Format.Base;
+using PlurCrawler.Search.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlurCrawler.Format
+{
+    public class JsonFormat<TResult> : BaseFormat<TResult, string> where TResult : ISearchResult
+    {
+        public override string Formatting(TResult resultData)
+        {
+            return JsonConvert.SerializeObject(resultData);
+        }
+
+        public bool SaveFile(string fileLocation, string data)
+        {
+            try
+            {
+                var sw = new StreamWriter(fileLocation, false);
+                sw.Write(data);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
