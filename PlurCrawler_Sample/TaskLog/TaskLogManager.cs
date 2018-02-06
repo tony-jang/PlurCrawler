6e41d005568afd7530783085dabc3545314f636a
Plurdis <uutak2000@naver.com>
2018-02-06 13:29:32 +0900

    작업 로그 관리 클래스 추가

---
+++ PlurCrawler_Sample/TaskLog/TaskLogManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PlurCrawler_Sample.TaskLog
+{
+    public class TaskLogManager
+    {
+        public TaskLogManager()
+        {
+            // TODO: Implement
+        }
+
+        public void ConnectFile(string FileName)
+        {
+            // TODO: Implement
+        }
+
+        public void ConnectListBox(ListBox listBox)
+        {
+            // TODO: Implement
+        }
+
+        public void WriteLine()
+        {
+            // TODO: Implement
+        }
+        
+        public void AddLog(string message, TaskLogType type)
+        {
+            // TODO: Implement
+        }
+    }
+}
---
+++ PlurCrawler_Sample/TaskLog/TaskLogType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlurCrawler_Sample.TaskLog
+{
+    public enum TaskLogType
+    {
+        System,
+        SearchReady,
+        Searching,
+        SearchFailed,
+    }
+}
