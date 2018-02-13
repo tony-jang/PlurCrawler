be8f5b511ce599445b2b458e0996b6a38c947afb
Plurdis <uutak2000@naver.com>
2018-02-13 17:37:30 +0900

    작업 보고서 틀 추가

---
+++ PlurCrawler_Sample/Report/SearchResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlurCrawler_Sample.Report
+{
+    public enum SearchResult
+    {
+        Success,
+        Fail_InvaildSetting,
+        Fail_APIError,
+    }
+}
---
+++ PlurCrawler_Sample/Report/TaskReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PlurCrawler.Search;
+using PlurCrawler.Search.Base;
+
+namespace PlurCrawler_Sample.Report
+{
+    public class TaskReport
+    {
+        public DateTime SearchDate { get; set; }
+
+        public int SearchCount { get; set; }
+
+        public SearchResult SearchResult { get; set; }
+        
+        public ServiceKind RequestService { get; set; }
+
+        public IEnumerable<ISearchResult> SearchData { get; set; }
+    }
+}
